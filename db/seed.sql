1397901ae1642f25b098faf4744ae8e9d71b573b
Michael J. Bruderer <mjb@migadu.ch>
2025-05-29 10:19:54 -0300

    Wip on seed

--- db/seed.sql
+++ db/seed.sql
@@ -1,4 +1,7 @@
 -- Test user for development "user@domain.com" with password "password"
-INSERT into users (username, password) values ('user@domain.com', '$2a$10$59jW86pmlBLK2CF.hqmNpOWDPFRPKLWm4u6mpP/p.q1gtH3P0sqyK') ON CONFLICT (username) DO NOTHING;
+INSERT into users (id) values (1) ON CONFLICT (id) DO NOTHING;
 -- Add another user, password created with iex>  Bcrypt.hash_pwd_salt("mysecret")
-INSERT into users (username, password) values ('test@test.net', '$2b$12$s4tZlMwI3JqJN0gVuGfRxOYA3cWZsCxSnioD5jgwODgJ6S5u6Kdt6') ON CONFLICT (username) DO NOTHING;
+INSERT into passwords (user_id, username, password) values (1, 'test@test.net', '$2b$12$s4tZlMwI3JqJN0gVuGfRxOYA3cWZsCxSnioD5jgwODgJ6S5u6Kdt6') ON CONFLICT (username) DO NOTHING;
+-- INSERT into users (username, password) values ('user@domain.com', '$2a$10$59jW86pmlBLK2CF.hqmNpOWDPFRPKLWm4u6mpP/p.q1gtH3P0sqyK') ON CONFLICT (username) DO NOTHING;
+-- -- Add another user, password created with iex>  Bcrypt.hash_pwd_salt("mysecret")
+-- INSERT into users (username, password) values ('test@test.net', '$2b$12$s4tZlMwI3JqJN0gVuGfRxOYA3cWZsCxSnioD5jgwODgJ6S5u6Kdt6') ON CONFLICT (username) DO NOTHING;
